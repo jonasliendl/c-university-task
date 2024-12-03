7de934c94661b6ba4aec21427a5799e0de5eca1b
jonasliendl <57270170+jonasliendl@users.noreply.github.com>
2024-12-03 15:38:15 +0100

    final changes

--- lib/par/par.h
+++ lib/par/par.h
@@ -7,30 +7,4 @@
 #define LOG_FILE "parallel.log"
 #define NUM_THREADS 4
 
-/*
-Mutex for logging
-*/
-extern pthread_mutex_t log_mutex;
-
-/*
-Thread arguments
-*/
-typedef struct {
-    Node* sublist;
-    int thread_num;
-} ThreadArgs;
-
-
-/*
-Diese Funktion verbindet zwei sortierte Listen. Sie nimmt zwei Zeiger auf die Listenköpfe 
-und gibt einen Zeiger auf die Kopfzeiger der zusammengeführten Liste zurück.
-*/
-Node* merge_sorted_lists(Node* list1, Node* list2);
-
-/*
-Diese Funktion wird von jedem Thread aufgerufen. Sie nimmt einen Zeiger auf ein ThreadArgs-Objekt.
-Sie gibt einen Zeiger auf die sortierte Liste zurück. Wenn ein Fehler auftritt, wird NULL zurückgegeben.
-*/
-void* ThrdFunc(void* arg);
-
 #endif
